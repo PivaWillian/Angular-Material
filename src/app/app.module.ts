08fa1b6ca192578fcb5e67b12ba93b8b0c37de8c
Willian Piva <willian.piva@gmail.com>
2023-03-10 17:32:43 -0300

    Some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{ Routes, RouterModule } from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { FormsModule } from '@angular/forms';
+import { ButtonsComponent } from './demo/demo/buttons/buttons.component';
 
+const routes: Routes = [
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
